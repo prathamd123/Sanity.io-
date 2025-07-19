bbcf6a8aec5b041c16467dd27190386ea1b3ad71
Pratham devatwal <84966285+prathamd123@users.noreply.github.com>
2025-07-19 13:58:10 +0530

    fix

--- blog-frontend/src/sanity.js
+++ blog-frontend/src/sanity.js
@@ -1,8 +1,10 @@
 import { createClient } from '@sanity/client';
 
+
+
 export default createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: process.env.REACT_APP_SANITY_DATASET,
+  projectId: import.meta.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: import.meta.env.REACT_APP_SANITY_DATASET,
   useCdn: true, // set to false if you want fresh data every time
-  apiVersion: process.env.REACT_APP_SANITY_API_VERSION || '2023-01-01',
+  apiVersion: import.meta.env.REACT_APP_SANITY_API_VERSION || '2023-01-01',
 });
